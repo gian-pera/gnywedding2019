359d138e6c0fef9fcd75aa3d2f15e588dc935620
gian-pera <ustgian25@gmail.com>
2017-08-29 02:11:32 +0800

    finalized the design (Finally :D)

--- src/main/resources/js/index.js
+++ src/main/resources/js/index.js
@@ -1,3 +1,19 @@
-$(function(){
-  $('.parallax').parallax();
+$(function() {
+  $('.carousel').carousel({
+    indicators: true,
+    dist: -50,
+    padding: 50
+  });
+
+  var interval = 1000;
+  var transition_carousel = function() {
+    $('.carousel').carousel('next');
+  };
+
+  var carousel_interval = setInterval(transition_carousel, 5000);
+  $('.carousel-item').hover(function() {
+    clearInterval(carousel_interval);
+  }, function(){
+    carousel_interval = setInterval(transition_carousel, 5000);
+  });
 });
---
+++ src/main/resources/js/rsvp.js
@@ -0,0 +1,3 @@
+$(function() {
+  $('#rsvp').modal();
+});
