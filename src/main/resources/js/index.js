e45381996fe6f8bfbe22ce33d901e1456f896a31
Pera, Giancarlo (gp0289) <gp0289@att.com>
2017-12-29 05:18:11 +0800

    Ensured that images are pre-loaded

--- src/main/resources/js/index.js
+++ src/main/resources/js/index.js
@@ -1,6 +1,6 @@
 $(function() {
 
-  $(window).load(function () {
+  $(window).on('load', function () {
     $('#preloader').delay(2000).fadeOut('slow', function() {
       $(this).remove();
     });
