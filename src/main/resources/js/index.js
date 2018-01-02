3d0fce00a6f85032ebf405690c7c393777b18aa5
Pera, Giancarlo (gp0289) <gp0289@att.com>
2018-01-03 05:54:41 +0800

    Reverted the scroll lock

--- src/main/resources/js/index.js
+++ src/main/resources/js/index.js
@@ -2,28 +2,8 @@ $(function() {
 
   $(window).on('load', function () {
     $('#preloader').delay(2000).fadeOut('slow', function() {
-      $('body').css('overflow-y', 'scroll');
       $(this).remove();
     });
   });
 
-/*
-  $('.carousel').carousel({
-    indicators: true,
-    dist: -50,
-    padding: 50
-  });
-
-  var interval = 1000;
-  var transition_carousel = function() {
-    $('.carousel').carousel('next');
-  };
-
-  var carousel_interval = setInterval(transition_carousel, 5000);
-  $('.carousel-item').hover(function() {
-    clearInterval(carousel_interval);
-  }, function(){
-    carousel_interval = setInterval(transition_carousel, 5000);
-  });
-*/
 });
