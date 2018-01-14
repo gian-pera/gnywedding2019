b1e698329c4b0137f4e8f35e629003ea55a37cd4
Pera, Giancarlo (gp0289) <gp0289@att.com>
2018-01-14 15:44:31 +0800

    Migrated website to use QBootstrap's template

--- src/main/server.js
+++ src/main/server.js
@@ -1,10 +1,8 @@
 'use strict'
 
-const _ = require('lodash')
 const bodyParser = require('body-parser')
 const express = require('express')
 const methodOverride = require('method-override')
-const moment = require('moment-timezone')
 
 const ApiError = require('./utils/api-error')
 const Logger = require('./utils/logger')
@@ -17,9 +15,9 @@ app.set('views', `${__dirname}/views`)
 app.set('view engine', 'pug')
 
 app.use('/', express.static(`${__dirname}/resources`))
-app.use('/', express.static('node_modules/font-awesome'))
-app.use('/', express.static('node_modules/materialize-css/dist'))
-app.use('/js', express.static('node_modules/jquery/dist'))
+//app.use('/', express.static('node_modules/font-awesome'))
+//app.use('/', express.static('node_modules/materialize-css/dist'))
+//app.use('/js', express.static('node_modules/jquery/dist'))
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -28,12 +26,6 @@ app.use(bodyParser.json())
 app.use(methodOverride())
 
 app.use('/', BaseRouter)
-
-app.get('/rest/v1/ping', function (req, res) {
-  res.json({
-    'date': moment(new Date()).format('YYYY-MM-DD HH:mm:ss zz [(GMT]ZZ[)]')
-  })
-})
 
 app.use(function (req, res) {
   res.status(404).json({
